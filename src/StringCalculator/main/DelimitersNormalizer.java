db1ddf28010dc9b3d3e2cb70f345bc01d70131ee
Manuel Pancorbo <manuelpancorbo90@gmail.com>
2016-02-25 23:54:07 +0100

    Added test to cover when the input has a custom delimiter

--- src/StringCalculator/main/DelimitersNormalizer.java
+++ src/StringCalculator/main/DelimitersNormalizer.java
@@ -1,16 +1,52 @@
 package StringCalculator.main;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class DelimitersNormalizer {
 
     private static final String[] DEFAULT_DELIMITERS = {",", "\n"};
+    private static final String DELIMITERS_LINE_START = "//";
+    private static final String DELIMITERS_LINE_END = "\n";
 
     public String normalizeDelimiters(String numbers, String normalizerDelimiter) {
         String normalizedNumbers = numbers;
+        List<String> delimiterList = getDelimiters(numbers);
+        normalizedNumbers = removeDelimitersLine(normalizedNumbers);
 
-        for (String delimiter : DEFAULT_DELIMITERS) {
+        for (String delimiter : delimiterList) {
             normalizedNumbers = normalizedNumbers.replace(delimiter, normalizerDelimiter);
         }
 
         return normalizedNumbers;
     }
+
+    private List<String> getDelimiters(String numbers){
+        List<String> delimitersList = new LinkedList<>(Arrays.asList(DEFAULT_DELIMITERS));
+
+        if (isThereCustomDelimiter(numbers)) {
+            delimitersList.add(getCustomDelimiter(numbers));
+        }
+
+        return delimitersList;
+    }
+
+    private boolean isThereCustomDelimiter(String numbers) {
+        return numbers.startsWith(DELIMITERS_LINE_START);
+    }
+
+    private String getCustomDelimiter(String numbers) {
+        return numbers.substring(2,3);
+    }
+
+    private String removeDelimitersLine(String numbers) {
+        if(!isThereCustomDelimiter(numbers)) {
+            return numbers;
+        }
+
+        int endOfDelimitersLineIndex = numbers.indexOf(DELIMITERS_LINE_END) + DEFAULT_DELIMITERS.length - 1;
+        return numbers.substring(endOfDelimitersLineIndex);
+    }
+
 }
--- src/StringCalculator/test/DelimitersNormalizerTest.java
+++ src/StringCalculator/test/DelimitersNormalizerTest.java
@@ -14,4 +14,9 @@ public class DelimitersNormalizerTest {
     public void should_replace_commas_and_eols_by_dollar_symbol() {
         assertEquals("1$2$3", delimitersNormalizer.normalizeDelimiters("1,2\n3", "$"));
     }
+
+    @Test
+    public void should_replace_numbers_delimiters_by_dollar_symbol() {
+        assertEquals("1$2$3$4", delimitersNormalizer.normalizeDelimiters("//*\n1*2,3\n4", "$"));
+    }
 }
